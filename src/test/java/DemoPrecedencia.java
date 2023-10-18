f9c517f757aa2c205870ab58733274ec743f2fb0
TC <tc@MacBook-Pro-de-TC.local>
2023-10-18 11:27:59 -0300

    primer commit

---
+++ src/test/java/DemoPrecedencia.java
@@ -0,0 +1,60 @@
+import org.testng.annotations.*;
+
+public class DemoPrecedencia {
+    @BeforeMethod
+    public void beforeMethod() {
+        System.out.println("beforeMethod()");
+    }
+    @AfterMethod
+    public void afterMethod() {
+        System.out.println("afterMethod()");
+    }
+    @DataProvider
+    public Object[][] Generador() {
+        System.out.println("Genero casos");
+        return new Object[][] {
+                new Object[] { 1, "a" },
+                new Object[] { 2, "b" },
+        };
+    }
+    @Test(dataProvider = "Generador")
+    public void TestGenerado(Integer n, String s) {
+        System.out.println("El generador provee: "+s+" -"+" - "+n);
+    }
+    @BeforeClass
+    public void beforeClass() {
+        System.out.println("beforeClass");
+    }
+    @AfterClass
+    public void afterClass() {
+        System.out.println("afterClass()");
+    }
+    @BeforeTest
+    public void beforeTest() {
+        System.out.println("beforeTest()");
+    }
+    @AfterTest
+    public void afterTest() {
+        System.out.println("afterTest()");
+    }
+    @BeforeSuite
+    public void beforeSuite() {
+        System.out.println("beforeSuite()");
+    }
+    @AfterSuite
+    public void afterSuite() {
+        System.out.println("afterSuite()");
+    }
+    @Test
+    public void test1() {
+        System.out.println("Ejecuto Test1");
+    }
+    @Test
+    public void test2() {
+        System.out.println("Ejecuto Test2");
+    }
+    @Test
+    public void testSinEmplementar() {
+        throw new RuntimeException("Este test aun no esta implementado");
+    }
+}
---
+++ src/test/java/TestPersona.java
@@ -0,0 +1,64 @@
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class TestPersona {
+    @DataProvider
+    public Persona[] GeneradorPersona() {
+        System.out.println("Generacion de casos");
+        return new Persona[]{
+                new Persona("Juan", "26.150.235", "1979-01-01", 42, true),
+                new Persona("Pedro", "27.280.234", "1980-02-01", 41, true),
+                new Persona("Maria", "28.184.259", "1981-03-01", 47, true),
+                new Persona("Cecilia", "32.234.528", "1983-04-01", 38, true),
+                new Persona("Carlos", "33.124.235", "1985-04-01", 36, true),
+                new Persona("Jose", "35.345.534", "1987-04-01", 34, true)
+        };
+    }
+
+    @Test(dataProvider = "GeneradorPersona")
+    public void testEdadBienCalculada(Persona p) {
+        int edadReportada = p.getEdad();
+        int edadReal = getEdad(p.getFechaNacimiento());
+        Assert.assertTrue(edadReportada == edadReal);
+    }
+
+    public int getEdad(String fechaNacimiento) {
+        try {
+            // Crea un objeto SimpleDateFormat para parsear la fecha de nacimiento.
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+
+            // Parsea la fecha de nacimiento.
+            Date fechaNac = sdf.parse(fechaNacimiento);
+
+            // Obtiene la fecha actual.
+            Date fechaActual = new Date();
+
+            // Crea un objeto Calendar para calcular la diferencia entre las fechas.
+            Calendar calNac = Calendar.getInstance();
+            Calendar calActual = Calendar.getInstance();
+            calNac.setTime(fechaNac);
+            calActual.setTime(fechaActual);
+
+            // Calcula la diferencia de años.
+            int edad = calActual.get(Calendar.YEAR) - calNac.get(Calendar.YEAR);
+
+            // Verifica si el cumpleaños ya pasó este año.
+            if (calNac.get(Calendar.MONTH) > calActual.get(Calendar.MONTH) ||
+                    (calNac.get(Calendar.MONTH) == calActual.get(Calendar.MONTH) &&
+                            calNac.get(Calendar.DAY_OF_MONTH) > calActual.get(Calendar.DAY_OF_MONTH))) {
+                edad--;
+            }
+            return edad;
+        } catch (ParseException e) {
+            e.printStackTrace();
+            return -1; // Manejo de errores
+        }
+    }
+}
