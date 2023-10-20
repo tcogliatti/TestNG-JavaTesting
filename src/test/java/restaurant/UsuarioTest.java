f64584dc9c70eaeeb82b7fbb5f0aab8c87177512
TC <tc@MacBook-Pro-de-TC.local>
2023-10-19 23:13:11 -0300

    configuracion de archivo suite testng.xml

--- src/test/java/restaurant/UsuarioTest.java
+++ src/test/java/restaurant/UsuarioTest.java
@@ -2,19 +2,21 @@ package restaurant;
 
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import restaurant.exceptions.EmailFormatException;
 import restaurant.exceptions.SinSaldoException;
 
 import static org.testng.Assert.*;
 
 public class UsuarioTest {
-    private Usuario
+    private Usuario usuario;
     @BeforeClass
-    public void loadPersona(){
-
+    public void loadPersona() throws EmailFormatException {
+        this.usuario = new Usuario("Jorge Almiron", 2,100,"jorge.almiron@gmail.com");
     }
     @Test(expectedExceptions = SinSaldoException.class)
-    public void testDescontarSaldo() {
-
+    public void testDescontarSaldo() throws SinSaldoException {
+        Float monto = this.usuario.getSaldo() + 100;
+        this.usuario.descontarSaldo(monto);
     }
 
 
